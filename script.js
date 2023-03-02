139d668437d51d384e9113891b3ae02c5d5c1bc5
Marco <patrickstar1281@yahoo.com>
2023-03-02 09:53:51 -0600

    player can move left & right

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function() {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = 800;
-    canvas.height = 120;
+    canvas.height = 720;
 
     class InputHandler {
         constructor() {
@@ -27,7 +27,36 @@ window.addEventListener('load', function() {
     }
 
     class Player {
-
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 200;
+            this.height = 200;
+            this.x = 0;
+            this.y = this.gameHeight - this.height;
+            //below same as "this.image = document.getElementById('playerImage')"
+            this.image = playerImage;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.speed = 0;
+        }
+        draw(context) {
+            context.fillStyle = 'white';
+            context.fillRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, 
+                this.height, this.x, this.y, this.width, this.height);
+        }
+        update(input) {
+            //horizontal movement
+            this.x += this.speed;
+            if (input.keys.indexOf('ArrowRight') > -1) {
+                this.speed = 5;
+            } else if (input.keys.indexOf('ArrowLeft') > -1) {
+                this.speed = -5;
+            } else {
+                this.speed = 0;
+            }
+        }
     }
 
     class Background {
@@ -47,8 +76,13 @@ window.addEventListener('load', function() {
     }
 
     const input = new InputHandler();
+    const player = new Player(canvas.width, canvas.height);
 
     function animate() {
-
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        player.draw(ctx);
+        player.update(input);
+        requestAnimationFrame(animate);
     }
-})
+    animate();
+});
